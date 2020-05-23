e470b641bdeb2280698bf835914d147719db6d16
Amy <Amy.l88@yahoo.com>
2020-05-22 22:53:56 -0500

    first commit

---
+++ Js-Kata2.js
@@ -0,0 +1,28 @@
+//KATA 01
+//Write a function named "add" which takes two arguments (as input variables) and returns their sum.
+function add(x, y) {
+    return x + y
+}
+console.log(add(2, 4))
+
+//KATA 02
+//Write a function named "multiply" which takes two arguments (as input variables) and returns their product.
+function multiply(x, y){
+    return ("i").repeat(x).repeat(y).length
+
+}
+console.log(multiply(6, 4))
+
+//KATA 03
+//Write a function named "power" which takes two arguments ( x and n) and returns the the result of raising x to the nth power.
+function power(x, n){
+    return Math.pow(x, n)
+}
+console.log(power(2, 8))
+console.log(power(3, 4))
+
+//KATA 04
+//Write a function named "factorial" which takes a single argument and returns the factorial of that value.
+function factorial(num){
+    
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Js-Katas-Activity-2
